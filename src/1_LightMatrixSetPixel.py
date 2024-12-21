29c3c0ea354868ebdf5bb765c5f1d444523d8e95
TomoniCodeDevelopers <tomonidevelopers@gmail.com>
2024-12-21 20:01:09 +0900

    Create 1_LightMatrixSetPixel.py

---
+++ src/1_LightMatrixSetPixel.py
@@ -0,0 +1,15 @@
+from spike import PrimeHub
+
+hub = PrimeHub()
+
+x = 1
+y = 4
+hub.light_matrix.set_pixel(x, y)
+
+x = 0
+y = 0
+hub.light_matrix.set_pixel(x, y)
+
+x = 4
+y = 4
+hub.light_matrix.set_pixel(x, y)
