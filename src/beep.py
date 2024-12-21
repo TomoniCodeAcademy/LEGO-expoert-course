0090069f5472ed4f1f0e8bc5e3d113734d98f8ad
TomoniCodeDevelopers <tomonidevelopers@gmail.com>
2024-12-21 19:45:42 +0900

    Create beep.py

---
+++ src/beep.py
@@ -0,0 +1,9 @@
+from spike import PrimeHub
+
+hub = PrimeHub()
+
+hub.speaker.beep(60, 0.2)
+hub.speaker.beep(64, 0.2)
+hub.speaker.beep(67, 0.5)
+hub.speaker.beep(64, 0.1)
+hub.speaker.beep(60, 0.1)
