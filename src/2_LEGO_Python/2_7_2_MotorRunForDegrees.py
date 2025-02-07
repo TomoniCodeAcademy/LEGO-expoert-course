5c63ce1b4ebd3b892680a82d32cbff2306dd10e4
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2025-02-07 11:57:11 +0900

    Update 2_7_2_MotorRunForDegrees.py

--- src/2_LEGO_Python/2_7_2_MotorRunForDegrees.py
+++ src/2_LEGO_Python/2_7_2_MotorRunForDegrees.py
@@ -3,7 +3,10 @@ from spike import Motor
 motor = Motor('E')
 motor.set_default_speed(50)
 motor.run_for_degrees(90)
+motor.get_position()
 
 #motor.run_for_degrees(-90)
 
+motor.run_to_position(90)
+motor.run_to_position(0)
 
