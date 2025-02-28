ddab5bef7afa5dccb5e59d50325c4ceda107fe4c
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2025-02-28 11:43:04 +0900

    Update 0_0_0_camera_tilt_sample.py

--- src/3_AICam_LEGO_Python/0_0_0_camera_tilt_sample.py
+++ src/3_AICam_LEGO_Python/0_0_0_camera_tilt_sample.py
@@ -6,8 +6,10 @@
 #
 #
 import sys
-from hub import port
 import time
+
+from hub import port
+from spike import Motor
 
 SCREEN_WIDTH =320
 SCREEN_HEIGHT =240
@@ -27,7 +29,7 @@ husky.send_CMD_REQ_ALGO(Algo.COLOR_RECOGNITION)
 
 
 motor = Motor('E')
-motor.set_default_speed(5)
+motor.set_default_speed(30)
 
 def move_camera_up(servo,degrees=10):
     servo.run_for_degrees(degrees)
@@ -40,15 +42,21 @@ while True:
     if len(blocks) > 0:
         (obj,id,x,y,w,h)=blocks[0]
         print('x', x, 'y', y, 'w', w, 'h', h)
-        if y > (CENTER_Y + 5): 
-            if y > 200:
+        if y > (CENTER_Y + 10): 
+            if y > 210:
+                print('down 30')
+                move_camera_down(motor,degrees=30)
+            elif y > 180:
                 print('down 20')
                 move_camera_down(motor,degrees=20)
             else:
                 print('down 10')
                 move_camera_down(motor,degrees=10)
-        elif y < (CENTER_Y - 5): 
-            if y < 40:
+        elif y < (CENTER_Y - 10): 
+            if y < 30:
+                print('up 30')
+                move_camera_up(motor,degrees=30)
+            elif y < 60:
                 print('up 20')
                 move_camera_up(motor,degrees=20)
             else:
